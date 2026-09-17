c55cf9c7eb11a7d90c64805af5413aa40f834ee6
maz1611307 <alizahidchaudhary307@gmail.com>
2026-09-17 20:57:48 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const chatHandler = require('./api/chat.js');
 
 app.use(express.json());
 
-// Serve static files like index.html
-app.use(express.static(path.join(__dirname)));
-
-// API route
+// Load API Handler
+const chatHandler = require('./api/chat.js');
 app.post('/api/chat', chatHandler);
 
-// Serve frontend on root route
+// Serve static frontend files
+app.use(express.static(path.join(__dirname)));
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
