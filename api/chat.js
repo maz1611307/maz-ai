8479878956dc9cecc19133cf48e83d3089554ef4
maz1611307 <alizahidchaudhary307@gmail.com>
2026-09-20 10:14:29 +0500

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -61,7 +61,7 @@ module.exports = async function handler(req, res) {
             "Content-Type": "application/json"
           },
           body: JSON.stringify({
-            model: "llama-3.1-8b-instant",
+            model: "llama-3.3-70b-versatile",
             messages: [{ role: "user", content: message }]
           })
         });
